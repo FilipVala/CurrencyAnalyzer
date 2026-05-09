729ded4a734c73474c9889bd5458dd756cc3d9d0
filipvala10 <filip.vala010@gmail.com>
2026-05-09 22:37:07 +0200

    feat: implement IExchangeRateService and ExchangeRateService

---
+++ CurrencyAnalyzer.Core/Interfaces/IExchangeRateService.cs
@@ -0,0 +1,15 @@
+﻿using CurrencyAnalyzer.Core.DTOs;
+using CurrencyAnalyzer.Core.Models;
+
+namespace CurrencyAnalyzer.Core.Interfaces;
+
+public interface IExchangeRateService
+{
+    Task<ExchangeRateResponse> GetLatestRatesAsync(string baseCurrency, IEnumerable<string> symbols);
+
+    Task<ExchangeRateResponse> GetHistoricalRatesAsync(string baseCurrency,
+        IEnumerable<string> symbols, DateTime startDate, DateTime endDate);
+
+    Task<Dictionary<string, decimal>> GetCurrentRatesForAnalysisAsync(string baseCurrency,
+        IEnumerable<string> selectedCurrencies);
+}
