fa55fb2e618c869fcb533295b08ea07a86e8d7d8
filipvala10 <filip.vala010@gmail.com>
2026-05-14 16:03:50 +0200

    Deploy ready

--- CurrencyAnalyzer.Core/Interfaces/IExchangeRateService.cs
+++ CurrencyAnalyzer.Core/Interfaces/IExchangeRateService.cs
@@ -4,5 +4,13 @@ namespace CurrencyAnalyzer.Core.Interfaces;
 
 public interface IExchangeRateService
 {
-    Task<ExchangeRateResponse> GetLatestRatesAsync(string baseCurrency, IEnumerable<string> symbols);
+    Task<ExchangeRateResponse> GetLatestRatesAsync(
+        string baseCurrency,
+        IEnumerable<string> symbols);
+
+    Task<ExchangeRateResponse> GetHistoricalRatesAsync(
+        string baseCurrency,
+        IEnumerable<string> symbols,
+        DateTime startDate,
+        DateTime endDate);
 }
